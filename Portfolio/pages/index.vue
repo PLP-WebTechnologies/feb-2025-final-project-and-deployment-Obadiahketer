71b92287c8b112ab09214c40458bcc8a78b6824f
Obadiahketer <kiptanuiobadia629@gmail.com>
2025-05-14 16:58:18 +0300

    Removed old git repo folder and added clean Portfolio version

---
+++ Portfolio/pages/index.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Features from '~/components/landing/Features.vue';
+import Gallery from '~/components/landing/Gallery.vue';
+
+definePageMeta({
+  layout: "landing",
+});
+</script>
+
+<template>
+  <LandingContainer>
+    <LandingHero></LandingHero>
+    <!-- <LandingFeatures></LandingFeatures> -->
+    <Gallery></Gallery>
+    <LandingLogos></LandingLogos>
+    <LandingCta></LandingCta>
+  </LandingContainer>
+</template>
